44026b07e78c9dc5125235b0d0bbfa133d26f6d1
Krzysztof Łakwa <fenrir14@gmail.com>
2018-08-03 11:40:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import PlayersList from './components/PlayersList';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      players: [
+        {
+          name: 'Kunegunda',
+          score: 5,
+        },
+        {
+          name: 'Hildebrand',
+          score: 0
+        }
+      ]
+    }
+  }
   render() {
     return(
       <div className="App">
+        <PlayersList players={this.state.players} />
       </div>
     );
   }
